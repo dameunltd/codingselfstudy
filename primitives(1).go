f7fa191bcc222a3ebe08e57f7e70651bc0440b91
Dame <50931631+dameunltd@users.noreply.github.com>
2019-11-26 15:01:58 -0500

    Learn Golang

---
+++ primitives(1).go
@@ -0,0 +1,27 @@
+package main
+
+import (
+		"fmt" 
+)	
+
+func main() {
+//		var n bool
+//		fmt.Printf("%v, %T\n", n, n)
+//		var n uint16 = 42
+//		fmt.Printf("%v, %T\n", n, n)
+
+/*Mathemtical problems
+a := 10
+b := 3
+fmt.Println(a+b)
+fmt.Println(a-b)
+fmt.Println(a*b)
+fmt.Println(a/b) // interger divison (remainder is dropped)
+fmt.Println(a%b)
+*/
+
+//var a int = 10
+// var b int8 = 3
+// fmt.Println(a+int(b)) // cannot add two integers of 2 different types (UPDATE: TYPE CONVERSION)
+
+}
---
+++ variables(2).go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var I int = 42 //Globally accessible throughout code
+
+func main() {
+
+		fmt.Println(I)
+}
