e4f064da8aa3221c9a87e78275dcefa5bcf5071b
Dame <50931631+dameunltd@users.noreply.github.com>
2019-11-29 12:14:42 -0500

    Constants(const) 11/29/19

---
+++ const(1).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Entity
+{
+private:
+    int m_X, m_Y;
+    mutable int var;
+public:
+    int GetX() const // function is not going to modify variables(read-only)
+    {
+        m_X = 2;
+        return m_X;
+    }
+
+    int GetX() // sometimes identical function set without *const*
+    {
+        return m_X;
+    }
+
+    void SetX(int x)
+    {
+        m_X = x;
+    }
+};
+
+void PrintEntity(const Entity& e) //pass entity through const reference(can't modify)
+{
+    std::cout<<e.GetX()<<std::endl;
+}
+int main()
+{
+    Entity e;
+
+    const int MAX_AGE = 90;
+
+    const int* const a = new int;
+
+    *a = 2;
+    a = (int*)&MAX_AGE;
+    a = nullptr;
+    std::cout<<*a<<std::endl;
+}
---
+++ const.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*Const is a promise to not change the variable it is set to.
+Promise helps simplify code
+*/
+
+int main()
+{
+    const int MAX_AGE = 101;
+
+    int* a = new int; // int a created on the heap to get pointer(new int)
+    const int* a = new int; // the contents of the pointer can't be altered
+    int* const a = new int; // same as 'int* a = new int' . const can be altered
+    const int* const a = new int; // pointer can be redirected but contents can't be altered
+
+    *a = 2;
+    a = (int*)&MAX_AGE;
+
+    std::cout<<*a<<std::endl;
+}
