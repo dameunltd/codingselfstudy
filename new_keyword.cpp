b44c220031f6c3dc2028744deaef975ac59ffef2
Dame <50931631+dameunltd@users.noreply.github.com>
2020-04-10 20:53:05 -0400

    C++ Code(4/10/2020)

---
+++ new_keyword.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using String = std::string;
+
+class Entity
+{
+private:
+    String m_Name;
+public:
+    Entity() : m_Name("Unknown") {}
+    Entity(const String& name) : m_Name(name) {}
+
+    const String& GetName() const { return m_Name; }
+ };
+
+int main()
+{
+    int a = 2;
+    int* b = new int[50]; // 200 bytes of memory (50x4bytes each)
+
+    Entity* e = new Entity(); // memory and constructor allocated
+
+    /*below code is same as line22 EXCEPT above calls Entity constructor, while
+    below allocates memory then gives pointer to memory, not calling constructor*/
+ //   Entity* e = (Entity*)malloc(sizeof(Entity));
+
+    delete e;
+    delete[] b; //when you create an array with new, use brackets when deleting
+
+    std::cin.get();
+}
---
+++ operator_overloading.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+struct Vector2
+{
+    float x, y;
+
+    Vector2(float x, float y) // vector2 is 2 component vector(two floats here)
+        : x(x), y(y) {}
+
+    Vector2 Add(const Vector2& other) const
+    {
+        return Vector2(x + other.x, y + other.y);
+    }
+
+    // this function is defining our addition operator in line36
+    Vector2 operator+(const Vector2& other) const
+    {
+        return Add(other);
+    }
+
+    Vector2 Multiply(const Vector2& other) const
+    {
+        return Vector2(x * other.x, y * other.y);
+    }
+
+    // this function is defining our multiplication operator in line36
+    Vector2 operator*(const Vector2& other) const
+    {
+        return Multiply(other);
+    }
+
+};
+
+// this function gives our "<<" operator the ability to print our Vector2 class functions
+std::ostream& operator<<(std::ostream& stream, const Vector2& other)
+{
+    stream << other.x << ", " << other.y;
+    return stream;
+}
+
+int main()
+{
+    Vector2 position(4.0f, 4.0f);
+    Vector2 speed(0.5f, 1.5f);
+    Vector2 powerup(1.1f, 1.1f);
+
+    Vector2 result1 = position.Add(speed.Multiply(powerup));
+    Vector2 result2 = position + speed * powerup;
+
+    std::cout << result2 << std::endl;
+    std::cin.get();
+}
