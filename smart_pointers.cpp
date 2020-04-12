dfe6353dd85cb725d9805b7a15a817935f841057
Dame <50931631+dameunltd@users.noreply.github.com>
2020-04-11 22:18:39 -0400

    C++ Code(4/11/2020)

---
+++ smart_pointers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Entity
+{
+public:
+    Entity()
+    {
+        std::cout<<"Created Entity!"<<std::endl;
+    }
+
+    ~Entity()
+    {
+        std::cout<<"Destroyed Entity!"<<std::endl;
+    }
+
+    void Print() {}
+};
+
+int main()
+{
+    {
+        std::shared_ptr<Entity> e0;
+        {
+ //         std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+
+            //when a shared pointer is assigned to another it increases reference count
+            std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+
+            /*when a shared pointer is assigned to a weak pointer, it doesnt increase
+            reference count*/
+            std::weak_ptr<Entity> weakEntity = sharedEntity;
+            e0 = sharedEntity;
+ //         entity->Print();
+        }
+    }
+    std::cin.get();
+}
